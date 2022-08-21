cda60c8c803d7cb64924ebf508fd75dd1a7ed656
Hameed Kunkanoor <41198503+Hameedkunkanoor@users.noreply.github.com>
2022-08-21 18:04:11 +0530

    Add project files.

---
+++ AzureDownloadAllBlobsFromContainer/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azure.Storage.Blobs;
+Console.WriteLine("Hello, World!");
+//the container name from which you want to download files from
+string containerName = "finditbuddy";
+//get connection string from Home -> Storage account -> Access keys(Under security + networking) -> Connection string
+string connectionString = "{place_your_connection_string}";
+
+BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+BlobContainerClient blobContainerClient = blobServiceClient.GetBlobContainerClient(containerName);
+var blobFiles = blobContainerClient.GetBlobs();
+foreach(var file in blobFiles)
+{
+    BlobClient blobClient=blobContainerClient.GetBlobClient(file.Name);
+    blobClient.DownloadTo(@"C:\Users\Lenovo\Downloads\Blobs\"+file.Name);
+}
+Console.ReadKey();
