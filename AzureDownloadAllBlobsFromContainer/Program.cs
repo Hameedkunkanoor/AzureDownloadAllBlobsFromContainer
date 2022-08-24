7c7d6964cffffc5fa446177796e533a6a72d12d2
Hameed Kunkanoor <41198503+Hameedkunkanoor@users.noreply.github.com>
2022-08-24 12:56:59 +0530

    Update Program.cs

--- AzureDownloadAllBlobsFromContainer/Program.cs
+++ AzureDownloadAllBlobsFromContainer/Program.cs
@@ -2,7 +2,7 @@
 using Azure.Storage.Blobs;
 Console.WriteLine("Hello, World!");
 //the container name from which you want to download files from
-string containerName = "finditbuddy";
+string containerName = "your_container_name";
 //get connection string from Home -> Storage account -> Access keys(Under security + networking) -> Connection string
 string connectionString = "{place_your_connection_string}";
 
